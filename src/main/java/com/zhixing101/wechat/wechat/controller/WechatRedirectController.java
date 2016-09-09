8b9f0725bece2115182241773c5af9491e05f40e
周智浩 <mikkomail@163.com>
2016-09-09 21:32:33 +0800

    jquery 3.1.0使用sinaapp公共库

--- src/main/java/com/zhixing101/wechat/wechat/controller/WechatRedirectController.java
+++ src/main/java/com/zhixing101/wechat/wechat/controller/WechatRedirectController.java
@@ -38,22 +38,6 @@ public class WechatRedirectController {
     @Value("#{configProperties['weixin.appId']}")
     private String appId;
 
-    // @RequestMapping(value = "redirectFindBook", method = RequestMethod.GET)
-    // public String redirectFindBook() throws UnsupportedEncodingException {
-    //
-    // String tmpUrl =
-    // webGrantRequestUrl.replaceFirst(Constants.STR_REDIRECT_URI,
-    // URLEncoder.encode(findBookUrl,
-    // Constants.STR_ENCODING_UTF_8).replace(Constants.STR_ASTERISK,
-    // Constants.STR_PER_CENT_TWO_A));
-    // tmpUrl = tmpUrl.replaceFirst(Constants.STR_SCOPE,
-    // Constants.STR_SNSAPI_USERINFO);
-    // tmpUrl = tmpUrl.replaceFirst(Constants.STR_STATE, "123");
-    // tmpUrl = Constants.STR_REDIRECT_COLON + tmpUrl;
-    // logger.info(tmpUrl);
-    // return tmpUrl;
-    // }
-    //
     @RequestMapping(value = "findBook", method = RequestMethod.GET)
     public String findBook(HttpServletRequest request, HttpServletResponse response) {
         return "findBook";
