02634165bba6bd3b7215f4ea4df4170054fadfa8
adam <bddiudiu@qq.com>
2016-09-24 15:52:26 +0800

    update

--- src/main/java/com/zhixing101/wechat/wechat/controller/HelloWorldController.java
+++ src/main/java/com/zhixing101/wechat/wechat/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.zhixing101.wechat.wechat.controller;
 
+import com.zhixing101.wechat.api.entity.Book;
+import com.zhixing101.wechat.api.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +15,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/hello")
 public class HelloWorldController {
 
+    @Autowired
+    BookService bookService;
+
     @RequestMapping(method = RequestMethod.GET)
     public String index() {
+
+        Book book = bookService.saveBookByISBN("9787515000541");
         return "index";
     }
 
