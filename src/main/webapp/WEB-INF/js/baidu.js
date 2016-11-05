44c142e6da7c604f9de6693c03be7c5e657fd5ab
周智浩 <mikkomail@163.com>
2016-11-05 08:40:12 +0800

    测试js

--- src/main/webapp/WEB-INF/js/baidu.js
+++ src/main/webapp/WEB-INF/js/baidu.js
@@ -31,9 +31,11 @@ function initMap(lng, lat, searchRadius, storagePlaceGeotableId) {
 			map.addControl(new BMap.NavigationControl());
 
 			// 存储微信getLocation函数获取到的经纬度
-			document.getElementById("globalLongtitude").value = point.lng;
-			document.getElementById("globalLatitude").value = point.lat;
-
+//			document.getElementById("globalLongtitude").value = point.lng;
+//			document.getElementById("globalLatitude").value = point.lat;
+			document.getElementById("globalLongtitude").value = 118.908363;
+			document.getElementById("globalLatitude").value = 31.915028;
+			
 			// 添加定位控件
 			// var geolocationControl = new BMap.GeolocationControl();
 			// geolocationControl.addEventListener("locationSuccess",
