dc9063dc65e06457327159ccfb8abd8ddb87ede4
周智浩 <mikkomail@163.com>
2016-11-28 21:20:08 +0800

    添加图片上传

--- src/main/webapp/WEB-INF/js/weixin.js
+++ src/main/webapp/WEB-INF/js/weixin.js
@@ -61,9 +61,19 @@ function authorityValidate(document) {
 				alert('用户拒绝授权获取地理位置');
 			}
 		});
-		
+
 		// 拍照或从手机相册中选图
-		
+		$("#uploaderInput").click(function() {
+			wx.chooseImage({
+				count : 1, // 默认9
+				sizeType : [ 'original', 'compressed' ], // 可以指定是原图还是压缩图，默认二者都有
+				sourceType : [ 'album', 'camera' ], // 可以指定来源是相册还是相机，默认二者都有
+				success : function(res) {
+					var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+				}
+			});
+		});
+
 		// 上传图片
 	});
 
