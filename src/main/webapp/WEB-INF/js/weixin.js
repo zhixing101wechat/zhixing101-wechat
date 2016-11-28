7b8356ffc1681659f34d45a1a819ab38c7858e3c
周智浩 <mikkomail@163.com>
2016-11-28 21:39:55 +0800

    update

--- src/main/webapp/WEB-INF/js/weixin.js
+++ src/main/webapp/WEB-INF/js/weixin.js
@@ -62,19 +62,6 @@ function authorityValidate(document) {
 			}
 		});
 
-		// 拍照或从手机相册中选图
-		$('#uploaderInput').on('click', function() {
-			wx.chooseImage({
-				count : 1, // 默认9
-				sizeType : [ 'original', 'compressed' ], // 可以指定是原图还是压缩图，默认二者都有
-				sourceType : [ 'album', 'camera' ], // 可以指定来源是相册还是相机，默认二者都有
-				success : function(res) {
-					var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-					syncUpload(localIds);
-				}
-			});
-		});
-
 		// 上传图片
 		var syncUpload = function(localIds) {
 			var localId = localIds.pop();
@@ -91,6 +78,19 @@ function authorityValidate(document) {
 				}
 			});
 		};
+
+		// 拍照或从手机相册中选图
+		$('#uploaderInput').on('click', function() {
+			wx.chooseImage({
+				count : 1, // 默认9
+				sizeType : [ 'original', 'compressed' ], // 可以指定是原图还是压缩图，默认二者都有
+				sourceType : [ 'album', 'camera' ], // 可以指定来源是相册还是相机，默认二者都有
+				success : function(res) {
+					var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+					syncUpload(localIds);
+				}
+			});
+		});
 	});
 
 	// 通过error接口处理失败验证
